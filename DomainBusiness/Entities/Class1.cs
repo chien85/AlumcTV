1f7b5b9871637deac8fe3bd7d669fd4a7731ea3b
chien85 <needmoney972@yahoo.com>
2015-02-13 16:16:49 +0700

    initital commit

---
+++ DomainBusiness/Entities/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainBusiness.Entities
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string ChannelId { get; set; }
+        public int Weight { get; set; }
+        public DateTime AdditiionDate { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+  
+    }
+    public class User
+    {
+
+    }
+    public class Role
+    {
+
+    }
+    public class Playlist
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public byte[] Icon{get;set;}
+        public int UserId{get;set;}
+        public string Description{get;set;}
+        public bool Lock{get;set;}
+        public string slug{get;set;}
+
+    }
+    
+}
